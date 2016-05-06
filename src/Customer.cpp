bc8193b7ed152d1f83a704a4201c6ed7c6a78aed
Maxwell Kay <acarbinem1@gmail.com>
2016-05-06 13:41:32 -0400

    Fixed method, now builds

--- src/Customer.cpp
+++ src/Customer.cpp
@@ -30,28 +30,29 @@ void Customer::addRental(const Rental& rental) {
 }
 
 // calculates the amount for a rental
-double amountFor(const Rental& rental) const {
-  double thisAmount = 0;
-  switch(it->getVideo().getCode()) {
+double Customer::amountFor(const Rental& r) const {
 
-  case Video::REGULAR:
-    thisAmount += 2;
-    if (it->getDaysRented() > 2)
-      thisAmount += (it->getDaysRented() - 2) * 1.5;
-    break;
+    double thisAmount = 0;
+    switch(r.getVideo().getCode()) {
 
-  case Video::NEW_RELEASE:
-    thisAmount += it->getDaysRented() * 3;
-    break;
+        case Video::REGULAR:
+        thisAmount += 2;
+        if (r.getDaysRented() > 2)
+            thisAmount += (r.getDaysRented() - 2) * 1.5;
+        break;
 
-  case Video::CHILDRENS:
-    thisAmount += 1.5;
-    if (it->getDaysRented() > 3)
-      thisAmount += (it->getDaysRented() - 3) * 1.5;
-    break;
-  }
-  
-  return thisAmount;
+        case Video::NEW_RELEASE:
+        thisAmount += r.getDaysRented() * 3;
+        break;
+
+        case Video::CHILDRENS:
+        thisAmount += 1.5;
+        if (r.getDaysRented() > 3)
+            thisAmount += (r.getDaysRented() - 3) * 1.5;
+        break;
+    }
+
+	return thisAmount;
 }
 
 // customer rental statement
@@ -80,10 +81,8 @@ std::string Customer::statement() const {
         result += it->getVideo().getTitle();
         result += "\t";
 
-        double thisAmount = 0;
-        thisAmount = amountFor(*it)
-
         // amount of rental
+        double thisAmount = amountFor(*it);
         std::ostringstream out_str_stream;
         out_str_stream << thisAmount;
         result += out_str_stream.str();
@@ -91,9 +90,7 @@ std::string Customer::statement() const {
 
         totalAmount += thisAmount;
     }
-    
-    thisAmount = amountFor(*it);
-    
+
     // total amount owed
     result += "Amount owed is: ";
     std::ostringstream out_str_stream;
