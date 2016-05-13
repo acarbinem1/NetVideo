a047f1b9cafe46b2e20319a2742a6eb9d8496423
Maxwell Kay <acarbinem1@gmail.com>
2016-05-12 22:56:49 -0400

    Added another movie to NetVideo

--- src/NetVideo.cpp
+++ src/NetVideo.cpp
@@ -15,15 +15,18 @@ int main() {
     // Videos
     Video lotr("Lord of the Rings", Video::REGULAR);
     Video hp("Harry Potter", Video::CHILDRENS);
+    Video tt("Titanic", Video::REGULAR);
 
     // Rentals of these movies
     Rental r1(lotr, 10);
     Rental r2(hp, 5);
+    Rental r3(tt, 7);
 
     // Customer with some rentals
     Customer customer("Fred");
     customer.addRental(r1);
     customer.addRental(r2);
+    customer.addRental(r3);
 
     // Output customer statement
     std::cout << customer.statement();
